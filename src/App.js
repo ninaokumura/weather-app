5af59944d50249b114d7397afeb2c2f7310eb316
Nina Okumura <elaineokumura@gmail.com>
2022-02-24 10:28:20 +1300

    initial wireframe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='weather-container'>
+        <h1>Weather</h1>
+        <div className='input-container'>
+          <input className='input' type='text' name='search' value='' />
+          <button>Search</button>
+        </div>
+      </div>
+      <div className='display-data'>Weather info goes here</div>
     </div>
   );
 }
