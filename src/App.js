209abfda75930bc1f81290e00eaef9b86b8f50be
Nina Okumura <elaineokumura@gmail.com>
2022-02-24 17:02:56 +1300

    add initial style with Tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useMemo } from 'react';
+
 // import { BsSunrise, BsSunset } from 'react-icons/bs';
-import './App.css';
+// import './App.css';
 
 // const icons = {};
 
@@ -44,33 +45,48 @@ function App() {
   }
 
   return (
-    <div className='App'>
-      <div className='weather-container'>
-        <h1>Weather</h1>
-        <div className='input-container'>
-          <input
-            onChange={handleChange}
-            className='input'
-            type='text'
-            name='search'
-            value={search}
-            placeholder='City...'
-          />
-          <button onClick={handleClick}>Search</button>
+    <div className='h-screen flex flex-col items-center justify-center bg-yellow-200 antialiased'>
+      <div className='grid gap-8'>
+        <h1 className='text-purple-800 text-8xl font-bold text-center'>
+          Weather
+        </h1>
+
+        <div className='flex w-full justify-center'>
+          <div className=' max-w-min flex overflow-hidden rounded-l-full rounded-r-full scale-125'>
+            <input
+              onChange={handleChange}
+              className='border-none'
+              type='search'
+              name='search'
+              value={search}
+              placeholder='City. e.g. Auckland'
+            />
+            <button
+              className='bg-purple-800 text-white text-xl h-full px-4'
+              onClick={handleClick}
+            >
+              Search
+            </button>
+          </div>
         </div>
       </div>
       <div className='display-data'>
-        <h2>
-          {city}
-          <span> {weatherResult.country}</span>
-        </h2>
-        <h3>{weatherResult.description}</h3>
-        <h3>{weatherResult.temp}</h3>
-        <h3>{weatherResult.feelsLike}</h3>
-        <h3>{weatherResult.tempMin}</h3>
-        <h3>{weatherResult.tempMax}</h3>
-        <h3>{weatherResult.sunrise}</h3>
-        <h3>{weatherResult.sunset}</h3>
+        <div className='data-name-container'>
+          <h2 className='data-title'>
+            {city}
+            <span> {weatherResult.country}</span>
+          </h2>
+        </div>
+        <div className='data-info'>
+          <h3>{weatherResult.description}</h3>
+          <h3>{weatherResult.temp}</h3>
+          <h3>{weatherResult.feelsLike}</h3>
+
+          <h3>{weatherResult.tempMin}</h3>
+          <h3>{weatherResult.tempMax}</h3>
+          <h3>{weatherResult.sunrise}</h3>
+          <h3>{weatherResult.sunset}</h3>
+        </div>
       </div>
     </div>
   );
